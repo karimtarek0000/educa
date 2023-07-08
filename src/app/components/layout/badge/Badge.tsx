b4d0aff93954950f9124a522ca56fb5c4a999020
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2023-07-08 23:40:03 +0300

    Prepare

---
+++ src/app/components/layout/badge/Badge.tsx
@@ -0,0 +1,9 @@
+const Badge = (): JSX.Element => {
+  return (
+    <>
+      <p>Lorem ipsum dolor sit amet.</p>
+    </>
+  );
+};
+
+export default Badge;
--- src/app/components/layout/navbar/Navbar.tsx
+++ src/app/components/layout/navbar/Navbar.tsx
@@ -9,3 +9,11 @@ const Navbar = (): JSX.Element => {
 };
 
 export default Navbar;
+
+/*
+<svg width="57" height="46" viewBox="0 0 57 46" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M5 5H51.7403" stroke="#292E37" stroke-width="9" stroke-linecap="round"/>
+<path d="M20 23H51.7403" stroke="#292E37" stroke-width="9" stroke-linecap="round"/>
+<path d="M9 41H51.7403" stroke="#292E37" stroke-width="9" stroke-linecap="round"/>
+</svg>
+*/
