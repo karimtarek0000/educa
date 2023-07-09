593c1228c38cf2f5d5ba18c28aa67d6659d3a6bf
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2023-07-09 23:12:54 +0300

    Header

--- src/app/components/layout/header/Header.tsx
+++ src/app/components/layout/header/Header.tsx
@@ -1,7 +1,55 @@
+import icon1 from "@/assets/icons/e-learning.svg";
+import Image from "next/image";
+import Style from "./style.module.css";
+import Link from "next/link";
+import RenderSVG from "../../shared/RenderSVG";
+import img1 from "@/assets/images/header/dashboard.png";
+
+const {
+  head,
+  desc,
+  linkGetStarted,
+  linkWatchOur,
+  wrapperLinks,
+  linkWatchOurIcon,
+  header,
+  headerWrapper,
+  headerCol1,
+  headerCol2,
+} = Style;
+
 const Header = (): JSX.Element => {
   return (
-    <header>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, cum?</p>
+    <header className={header}>
+      <div className={headerWrapper}>
+        <div className={headerCol1}>
+          <Image src={icon1} alt="e-learning" />
+          <h1 className={head}>
+            Dream big <br /> Do right.
+          </h1>
+          <p className={desc}>
+            Slite helps remote teams share ideas, save knowledge, and work
+            together anytime, any place.
+          </p>
+          <div className={wrapperLinks}>
+            <Link className={`${linkGetStarted} gradient-button`} href="/">
+              get started now
+            </Link>
+            <Link className={linkWatchOur} href="/">
+              <div className={linkWatchOurIcon}>
+                <RenderSVG name="play" size="1rem" />
+              </div>
+              watch our platform!
+            </Link>
+          </div>
+        </div>
+
+        <div className={headerCol2}>
+          <div className="max-lg:max-w-[50vw]">
+            <Image src={img1} alt="dashboard" />
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import SectionOne from "./components/landing/sectionOne/SectionOne";
+import Header from "./components/layout/header/Header";
 import Navbar from "./components/layout/navbar/Navbar";
 
 export default function Home() {
   return (
     <main className="relative min-h-screen overflow-hidden">
       <Navbar />
-      <SectionOne />
+      <Header />
     </main>
   );
 }
